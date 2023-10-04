7568a9c295b36c8447563345209978ba422bf30d
TanvirhCSE17 <tanvirhcse17@gmail.com>
2023-10-05 01:10:44 +0600

    details and go back button added

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const Details = () => {
     const details=useLoaderData();
     const {email,name,phone,website}=details;
+    const navigate=useNavigate();
+
+    const handlatGoBack=()=>navigate(-1);
     return (
         <div>
             <h2>Name: {name}</h2>
             <h3>Email: {email}</h3>
             <h3>Phone: {phone}</h3>
             <h4>Site: {website}</h4>
+            <button onClick={handlatGoBack}>Go Back</button>
         </div>
     );
 };
--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Friend = (props) => {
     const {k, friend}=props;
+    const navigate=useNavigate()
+
+    const handlarDetails=()=>{
+        navigate(`/friends/${k}`)
+    }
+
     return (
         <div style={{padding:'3px',backgroundColor: 'tomato', borderRadius:'4px', margin: '10px'}}>
             <Link  to={`/friends/${k}`}>{friend}</Link>
+            <button onClick={handlarDetails}>details</button>
         </div>
     );
 };
