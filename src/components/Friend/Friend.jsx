c6e092f7768dc990a312c25c0df6f3f60e2025ce
TanvirhCSE17 <tanvirhcse17@gmail.com>
2023-10-05 10:45:37 +0600

    updated active link

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -12,7 +12,7 @@ const Friend = (props) => {
     return (
         <div style={{padding:'3px',backgroundColor: 'tomato', borderRadius:'4px', margin: '10px'}}>
             <Link  to={`/friends/${k}`}>{friend}</Link>
-            <button onClick={handlarDetails}>details</button>
+            <div> <button onClick={handlarDetails}>details</button></div>
         </div>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <nav >
-            <Link style={{padding:'5px'}} to="/">home</Link>
-            <Link style={{padding:'5px'}} to="/friends">Friends</Link>
-            <Link style={{padding:'5px'}} to='/about'>About</Link>
-            <Link style={{padding:'5px'}} to='/contacts'>Contacts</Link>
-            <Link style={{padding:'5px'}} to='/login'>login</Link>
-        </nav>
+        // <nav >
+        //     <Link style={{padding:'5px'}} to="/">home</Link>
+        //     <Link style={{padding:'5px'}} to="/friends">Friends</Link>
+        //     <Link style={{padding:'5px'}} to='/about'>About</Link>
+        //     <Link style={{padding:'5px'}} to='/contacts'>Contacts</Link>
+        //     <Link style={{padding:'5px'}} to='/login'>login</Link>
+        // </nav>
+          <nav>
+            <NavLink to="/" className={({ isActive}) => isActive? "active": "",'common'}>Home</NavLink>
+            <NavLink to='/friends' className={({ isActive}) => isActive? "active": "",'common'}>Friends</NavLink>
+            <NavLink to='/about' className={({ isActive}) => isActive? "active": "",'common'}>About</NavLink>
+            <NavLink to='/contacts' className={({ isActive}) => isActive? "active": "",'common'}>Contacts</NavLink>
+            <NavLink to='/login' className={({ isActive}) => isActive? "active": "",'common'}>Login</NavLink>
+        
+          </nav>
     );
 };
 
